11f6f670a27a1faa533111e5101e1b4fad51c912
pitTech <aonsercs@gmail.com>
2019-11-12 14:40:25 +0700

    Second commit

--- src/main.js
+++ src/main.js
@@ -1,9 +1,19 @@
 import Vue from 'vue';
+import Buefy from 'buefy';
+import firebase from 'firebase';
 import App from './App.vue';
 import router from './router';
 
+Vue.use(Buefy);
 Vue.config.productionTip = false;
+const configOptions = {
+  apiKey: 'AIzaSyAWvINUgJeX1KpqPCpBCXeYW0vfzMGsfZA',
+  authDomain: 'room-reserve-17c64.firebaseapp.com',
+  databaseURL: 'https://room-reserve-17c64.firebaseio.com',
+  projectId: 'room-reserve-17c64',
+};
 
+firebase.initializeApp(configOptions);
 new Vue({
   router,
   render: h => h(App),
