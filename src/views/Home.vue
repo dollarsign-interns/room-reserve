c2d57a1837b9ae5812a5fb392a4d38dd8f47bf22
pitTech <aonsercs@gmail.com>
2019-11-12 14:50:40 +0700

    Thrid commit

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -7,12 +7,7 @@
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue';
-
 export default {
   name: 'home',
-  components: {
-    HelloWorld,
-  },
 };
 </script>
--- src/views/Login.vue
+++ src/views/Login.vue
@@ -39,7 +39,7 @@
                   />
                 </div>
               </div>
-
+              <br>
               <div class="form-group row mb-0">
                 <div class="col-md-8 offset-md-4">
                   <button type="submit" class="btn btn-primary">Login</button>
